af5ac3a2a27f8c527b9249593b525815ba92b698
EC2 Default User <ec2-user@ip-172-31-40-211.us-east-2.compute.internal>
2020-01-25 05:59:22 +0000

    before 12

---
+++ app/Http/Controllers/Admin/FoodController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FoodController extends Controller
+{
+    //
+    public function add() {
+        return view('admin.food.create');
+    }
+    
+    public function create() {
+        return redirect('admin/food/create');
+    }
+    
+    public function edit() {
+        return view('admin.food.edit');
+    }
+    
+    public function update() {
+        return redirect('admin/food/edit');
+    }
+}
